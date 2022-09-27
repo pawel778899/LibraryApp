ec1b1afa67e4d35e0728b86d470a2114634b1b8c
Pawel <pawel@pawel.com>
2022-09-27 22:06:33 +0200

    Library v1.2

---
+++ src/main/java/app/LibraryApp.java
@@ -0,0 +1,11 @@
+package app;
+
+public class LibraryApp {
+    private static final String APP_NAME = "Biblioteka v1.2";
+
+    public static void main(String[] args) {
+        System.out.println(APP_NAME);
+        LibraryControl libControl = new LibraryControl();
+        libControl.controlLoop();
+    }
+}
